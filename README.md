2bab774761e237277453314a96da8e8c18d9a2ae
KIMTHE <kcms2369@naver.com>
2022-02-26 01:43:26 +0900

    [lesson 6] : add record for implementing GET method in rest-api
    - GET method
    - parsing path variable
    - parsing query parameter

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # study_springboot
-코프링부트 공부내용
+
+- [kotlin으로 개발하는 spring boot web MVC](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8-%EC%BD%94%ED%8B%80%EB%A6%B0/dashboard)
+
+- 인프런에서 수강하는 스프링부트 강의 실습기록 입니다.
---
+++ mvc/src/main/kotlin/com/thekim/mvc/controller/get/GetApiController.kt
@@ -0,0 +1,61 @@
+package com.thekim.mvc.controller.get
+
+import com.thekim.mvc.controller.get.model.http.UserRequest
+import org.springframework.web.bind.annotation.*
+
+@RestController    // REST API Controller 동작
+@RequestMapping("/api") // http://localhost:8080/api
+class GetApiController {
+
+    @GetMapping(path = ["/hello"]) // GET http://localhost:8080/api/hello
+    fun hello(): String {
+        return "hello kotlin"
+    }
+
+    // 옛날 방식
+    @RequestMapping(method = [RequestMethod.GET], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping"
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}") // GET http://localhost:8080/api/get-mapping/path-variable/{name}
+    fun pathVariable(@PathVariable name: String, @PathVariable age: Int): String {
+        println("${name}, $age")
+        return "$name $age"
+    }
+
+    @GetMapping("/get-mapping/path-variable2/{name}/{age}") // GET http://localhost:8080/api/get-mapping/path-variable/{name}
+    fun pathVariable2(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") age: Int): String {
+        val name = "kotlin"
+
+        println("${_name}, $age")
+        return "$_name $age"
+    }
+
+    // http://localhost:8080/api/page?key=value&key=value
+    @GetMapping("/get-mapping/query-param") //?name=minsu&age=25
+    fun queryParam(
+        @RequestParam(name = "name") name: String,
+        @RequestParam(value = "age") age: Int
+    ): String {
+        println("$name, $age")
+        return "$name $age"
+    }
+
+    // name, age, address, email
+    // 자바에서는 변수명에 '-' 을 사용할 수 없다.
+    // phoneNumber -> phonenumber, phone-number
+    @GetMapping("get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String, Any>): Map<String, Any> {
+        println(map)
+        val phoneNumber = map["phone-number"]
+        return map
+
+    }
+}
